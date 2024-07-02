492594a0c3a98b7b082b999efe768b21cfb9bbd8
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-07-02 13:37:44 +0900

    theme

---
+++ lib/theme/color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Color(0xffFFDEA7);
+  static const Color secondary = Color(0xffBFA883);
+  static const Color tertiery = Color(0xff443E3E);
+  static const Color fourth = Color(0xff1C1B1F);
+  static const Color textbox = Color(0xffFFFFFF);
+  static const Color textboxs = Color(0xff6B6B6B);
+  static const Color background = Color(0xffFFFCF6);
+  static const Color button = Color(0xffF6DE83);
+  static const Color text = Color(0xff3F3B3B);
+  static const Color texts = Color(0xffA1A1A1);
+  static const Color textt = Color(0xff9D9D9D);
+  static const Color texth = Color(0xffACACAC);
+}
---
+++ lib/theme/text.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+final basicTheme = ThemeData(
+  textTheme: TextTheme(
+    displayLarge: TextStyle(fontFamily: 'Kangwonedu', fontSize: 24),
+  ),
+);
+
+const TextStyle b27 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.bold,
+  fontSize: 27,
+  color: Color(0xFF3F3B3B),
+);
+
+const TextStyle b23 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.bold,
+  fontSize: 23,
+  color: Color(0xFF3F3B3B),
+);
+
+const TextStyle b20 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+  color: Color(0xFF3F3B3B),
+);
+
+const TextStyle b18 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+  color: Color(0xFF3F3B3B),
+);
+
+const TextStyle b15 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.bold,
+  fontSize: 15,
+  color: Color(0xFF3F3B3B),
+);
+
+const TextStyle b15g = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.bold,
+  fontSize: 15,
+  color: Color(0xFFACACAC),
+);
+
+const TextStyle b13 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.bold,
+  fontSize: 13,
+  color: Color(0xFF3F3B3B),
+);
+
+
+
+const TextStyle l22 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.w400,
+  fontSize: 22,
+  color: Color(0xFF3F3B3B),
+);
+
+const TextStyle l17 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.w400,
+  fontSize: 17,
+  color: Color(0xFF3F3B3B),
+);
+
+const TextStyle l15 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.w400,
+  fontSize: 15,
+  color: Color(0xFF3F3B3B),
+);
+
+const TextStyle l15g = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.w400,
+  fontSize: 15,
+  color: Color(0xFF424242),
+);
+
+const TextStyle l14 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.w400,
+  fontSize: 14,
+  color: Color(0xFF3F3B3B),
+);
+
+const TextStyle l13 = TextStyle(
+  fontFamily: 'Kangwonedu',
+  fontWeight: FontWeight.w400,
+  fontSize: 13,
+  color: Color(0xFF3F3B3B),
+);
