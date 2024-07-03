a4be09cba46cf1b383be9c5501cc0dd75738461e
minchanpark <alscksqkr13@gmail.com>
2024-07-03 13:16:45 +0900

    stamp page 수정

--- lib/stamp.dart
+++ lib/stamp.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 
 class StampPage extends StatelessWidget {
+  const StampPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xffA3C2FF),
+        backgroundColor: const Color(0xffA3C2FF),
         title: Image.asset('assets/6/ssoup.png', width: 77, height: 77),
       ),
       body: Column(
         children: [
           ClipRRect(
-            borderRadius: BorderRadius.only(
+            borderRadius: const BorderRadius.only(
               bottomLeft: Radius.elliptical(200, 70),
               bottomRight: Radius.elliptical(200, 70),
             ),
             child: Container(
-              color: Color(0xffA3C2FF),
+              color: const Color(0xffA3C2FF),
               height: 200,
-              child: Row(
+              child: const Row(
                 children: [
                   Expanded(
                     child: Column(
@@ -55,15 +57,60 @@ class StampPage extends StatelessWidget {
               crossAxisCount: 3,
               padding: const EdgeInsets.all(16),
               children: [
-                StampItem(name: '무꼬기', image: 'assets/images/example.png'),
-                StampItem(name: '꽃부기', image: 'assets/images/example.png'),
-                StampItem(name: '베비샤크', image: 'assets/images/example.png'),
-                StampItem(name: '타꼬', image: 'assets/images/example.png'),
-                StampItem(name: '꾸래미', image: 'assets/images/example.png'),
-                StampItem(name: '젤리꼬기', image: 'assets/images/example.png'),
-                StampItem(name: '', image: 'assets/images/example.png'),
-                StampItem(name: '', image: 'assets/images/example.png'),
-                StampItem(name: '완주!', image: 'assets/images/example.png'),
+                StampItem(
+                  name: '무꼬기',
+                  image: 'assets/stamp_fish1.png',
+                  width: 179,
+                  height: 179,
+                ),
+                StampItem(
+                  name: '꽃부기',
+                  image: 'assets/stamp_fish2.png',
+                  width: 144,
+                  height: 144,
+                ),
+                StampItem(
+                  name: '베비샤크',
+                  image: 'assets/stamp_fish3.png',
+                  width: 165,
+                  height: 165,
+                ),
+                StampItem(
+                  name: '타꼬',
+                  image: 'assets/stamp_fish4.png',
+                  width: 119,
+                  height: 119,
+                ),
+                StampItem(
+                  name: '꾸래미',
+                  image: 'assets/stamp_fish5.png',
+                  width: 164,
+                  height: 164,
+                ),
+                StampItem(
+                  name: '젤리꼬기',
+                  image: 'assets/stamp_fish6.png',
+                  width: 126,
+                  height: 126,
+                ),
+                StampItem(
+                  name: '',
+                  image: 'assets/images/example.png',
+                  width: 144,
+                  height: 144,
+                ),
+                StampItem(
+                  name: '',
+                  image: 'assets/images/example.png',
+                  width: 144,
+                  height: 144,
+                ),
+                StampItem(
+                  name: '',
+                  image: 'assets/complete.png',
+                  width: 58,
+                  height: 21,
+                ),
               ],
             ),
           ),
@@ -76,8 +123,16 @@ class StampPage extends StatelessWidget {
 class StampItem extends StatelessWidget {
   final String name;
   final String image;
+  final double width;
+  final double height;
 
-  StampItem({required this.name, required this.image});
+  StampItem({
+    super.key,
+    required this.name,
+    required this.image,
+    required this.width,
+    required this.height,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -85,7 +140,12 @@ class StampItem extends StatelessWidget {
       children: [
         CircleAvatar(
           radius: 40,
-          backgroundImage: AssetImage(image),
+          child: Image.asset(
+            image,
+            width: width,
+            height: height,
+          ),
+          //backgroundImage: AssetImage(image),
         ),
         const SizedBox(height: 8),
         Text(
