2993582d1e0907adbb74da3abdb522e51729a025
minchanpark <alscksqkr13@gmail.com>
2024-07-16 13:22:42 +0900

    google api 중간 완료

--- lib/map.dart
+++ lib/map.dart
@@ -1,21 +1,45 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:ssoup/constants.dart';
 
 class GoogleMapPage extends StatefulWidget {
+  const GoogleMapPage({super.key});
+
   @override
   _GoogleMapPageState createState() => _GoogleMapPageState();
 }
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
   GoogleMapController? _mapController;
-  LatLng _currentPosition = const LatLng(37.46164222168521, 130.87823867797852);
+  LatLng _currentPosition = const LatLng(36.10155104193711, 129.39063285108818);
   final Location _location = Location();
+  final Set<Marker> _markers = {};
+  final LatLng _destinationLocation =
+      const LatLng(36.1047753, 129.3876298); // 변경된 도착지
+  final LatLng _startLocation =
+      const LatLng(36.10155104193711, 129.39063285108818);
+  final Set<Polyline> _polylines = {};
+  StreamSubscription<LocationData>? _locationSubscription;
 
   @override
   void initState() {
     super.initState();
     _checkPermissions();
+    _setInitialMarkers();
+    _getRoute();
+  }
+
+  @override
+  void dispose() {
+    _mapController?.dispose();
+    _locationSubscription?.cancel();
+    super.dispose();
   }
 
   Future<void> _checkPermissions() async {
@@ -38,18 +62,144 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
       }
     }
 
-    _location.onLocationChanged.listen((LocationData currentLocation) {
+    _locationSubscription =
+        _location.onLocationChanged.listen((LocationData currentLocation) {
+      if (!mounted) return;
       setState(() {
         _currentPosition =
             LatLng(currentLocation.latitude!, currentLocation.longitude!);
+        _updateCurrentLocationMarker();
         _mapController?.animateCamera(CameraUpdate.newLatLng(_currentPosition));
       });
     });
 
     final currentLocation = await _location.getLocation();
+    if (!mounted) return;
     setState(() {
       _currentPosition =
           LatLng(currentLocation.latitude!, currentLocation.longitude!);
+      _updateCurrentLocationMarker();
+    });
+  }
+
+  void _setInitialMarkers() {
+    if (!mounted) return;
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('sL'),
+          position: _startLocation,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          infoWindow: InfoWindow(title: 'Start Location'),
+        ),
+      );
+      _markers.add(
+        Marker(
+          markerId: MarkerId('dL'),
+          position: _destinationLocation,
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+          infoWindow: InfoWindow(title: 'Destination Location'),
+        ),
+      );
+    });
+  }
+
+  Future<void> _getRoute() async {
+    final String apiKey = googleMapsDirectionsApiKeys;
+    final String url =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=${_startLocation.latitude},${_startLocation.longitude}&destination=${_destinationLocation.latitude},${_destinationLocation.longitude}&mode=driving&key=$apiKey';
+
+    print('Request URL: $url'); // 요청 URL을 출력하여 확인합니다.
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      print('Response Data: $data'); // 응답 데이터를 출력하여 확인합니다.
+      if (data['routes'].isNotEmpty) {
+        final points = data['routes'][0]['overview_polyline']['points'];
+        _setPolylineFromPoints(points);
+      } else {
+        print('No routes found');
+      }
+    } else {
+      print('Failed to load directions: ${response.statusCode}');
+      print('Error Response: ${response.body}'); // 에러 응답 내용을 출력하여 확인합니다.
+    }
+  }
+
+  void _setPolylineFromPoints(String points) {
+    final List<LatLng> polylineCoordinates = _decodePolyline(points);
+    if (!mounted) return;
+    setState(() {
+      _polylines.add(
+        Polyline(
+          polylineId: PolylineId('route'),
+          points: polylineCoordinates,
+          color: Colors.black,
+          width: 5,
+        ),
+      );
+    });
+  }
+
+  List<LatLng> _decodePolyline(String polyline) {
+    List<LatLng> coordinates = [];
+    int index = 0;
+    int len = polyline.length;
+    int lat = 0;
+    int lng = 0;
+
+    while (index < len) {
+      int b;
+      int shift = 0;
+      int result = 0;
+
+      do {
+        b = polyline.codeUnitAt(index++) - 63;
+        result |= (b & 0x1F) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+
+      int dlat = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+      lat += dlat;
+
+      shift = 0;
+      result = 0;
+
+      do {
+        b = polyline.codeUnitAt(index++) - 63;
+        result |= (b & 0x1F) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+
+      int dlng = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+      lng += dlng;
+
+      coordinates.add(LatLng(
+        lat / 1E5,
+        lng / 1E5,
+      ));
+    }
+
+    print('Decoded Polyline Coordinates: $coordinates'); // 디코딩된 좌표를 출력하여 확인합니다.
+
+    return coordinates;
+  }
+
+  void _updateCurrentLocationMarker() {
+    if (!mounted) return;
+    setState(() {
+      _markers.removeWhere((marker) => marker.markerId == MarkerId('cL'));
+      _markers.add(
+        Marker(
+          markerId: MarkerId('cL'),
+          position: _currentPosition,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+          infoWindow: InfoWindow(title: 'Current Location'),
+        ),
+      );
     });
   }
 
@@ -57,18 +207,20 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google Map Page'),
+        title: const Text('Google Map Page'),
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: _currentPosition,
           zoom: 14.0,
         ),
+        markers: _markers,
         onMapCreated: (GoogleMapController controller) {
           _mapController = controller;
         },
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
+        polylines: _polylines,
       ),
     );
   }
