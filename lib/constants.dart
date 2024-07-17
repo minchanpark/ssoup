43a016e23102b3ac5e7ed145e10754c6d6f000e0
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-07-17 15:40:42 +0900

    d

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+const String googleMapsApiKey = "AIzaSyAO1LPcC2ct0IrFGNXUVZcMSZMxjojRPrw";
+const String naverClientId = "y1j4kgfwpv";
+const String naverClientSecret = "0Lr7EBoqCleZY1bnXinUcgug91iFTEuj6JNIsXI6";
