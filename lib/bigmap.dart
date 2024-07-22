dbf488e83877871cb89dc074ae8c7fea07e7d83a
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-07-22 10:43:35 +0900

    ㅇㅇㅇ

--- lib/bigmap.dart
+++ lib/bigmap.dart
@@ -19,71 +19,17 @@ class BigMapPage extends StatefulWidget {
 
 class _BigMapPageState extends State<BigMapPage> {
   GoogleMapController? _mapController;
-  LatLng _currentPosition = const LatLng(36.10155104193711, 129.39063285108818);
+  LatLng _currentPosition = const LatLng(37.49893355978079, 130.86866855621338);
   final Location _location = Location();
   final Set<Marker> _markers = {};
-  final LatLng _destinationLocation = const LatLng(36.1022665, 129.3913618);
-  final LatLng _startLocation = const LatLng(36.1047753, 129.3876298);
   final Set<Polyline> _polylines = {};
   StreamSubscription<LocationData>? _locationSubscription;
-
-  Future<void> _getNaverRoute() async {
-    final String url =
-        'https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${_startLocation.longitude},${_startLocation.latitude}&goal=${_destinationLocation.longitude},${_destinationLocation.latitude}&option=trafast';
-
-    final response = await http.get(
-      Uri.parse(url),
-      headers: {
-        'X-NCP-APIGW-API-KEY-ID': naverClientId,
-        'X-NCP-APIGW-API-KEY': naverClientSecret,
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      print('Response Data: $data');
-
-      final List<dynamic>? routes = data['route'] != null
-          ? data['route']['trafast'] ?? data['route']['traoptimal']
-          : null;
-
-      if (routes != null && routes.isNotEmpty) {
-        final points = routes[0]['path'];
-        _setPolylineFromNaverPoints(points);
-      } else {
-        print('No routes found');
-      }
-    } else {
-      print('Failed to load directions: ${response.statusCode}');
-      print('Error Response: ${response.body}');
-    }
-  }
-
-  void _setPolylineFromNaverPoints(List points) {
-    final List<LatLng> polylineCoordinates = points.map<LatLng>((point) {
-      return LatLng(point[1], point[0]);
-    }).toList();
-
-    if (!mounted) return;
-    setState(() {
-      _polylines.clear();
-      _polylines.add(
-        Polyline(
-          polylineId: const PolylineId('route'),
-          points: polylineCoordinates,
-          color: Colors.black,
-          width: 5,
-        ),
-      );
-    });
-  }
 
   @override
   void initState() {
     super.initState();
     _checkPermissions();
-    _setInitialMarkers();
-    _getNaverRoute();
+    _addMarkers();
   }
 
   @override
@@ -130,90 +76,7 @@ class _BigMapPageState extends State<BigMapPage> {
             LatLng(currentLocation.latitude!, currentLocation.longitude!);
         _updateCurrentLocationMarker();
         _mapController?.animateCamera(CameraUpdate.newLatLng(_currentPosition));
-
-        double distance = _calculateDistance(
-          _currentPosition.latitude,
-          _currentPosition.longitude,
-          _destinationLocation.latitude,
-          _destinationLocation.longitude,
-        );
-
-        if (distance <= 30) {
-          _showArrivalPopup(context);
-        }
       });
-    });
-  }
-
-  Future<Map<String, dynamic>> _fetchNotificationData() async {
-    DocumentSnapshot snapshot = await FirebaseFirestore.instance
-        .collection('stamp')
-        .doc('Tu1FQ3Q9hwZeCtGe67xQ')
-        .get();
-    return snapshot.data() as Map<String, dynamic>;
-  }
-
-  void _showArrivalPopup(BuildContext context) async {
-    Map<String, dynamic> stampDetail = await _fetchNotificationData();
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(stampDetail['stampName']),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Image.network(stampDetail['stampImageUrl']),
-              Text(stampDetail['location']),
-              Text(stampDetail['km']),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('확인'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  double _calculateDistance(
-      double lat1, double lon1, double lat2, double lon2) {
-    const p = 0.017453292519943295; // Math.PI / 180
-    final c = cos;
-    final a = 0.5 -
-        c((lat2 - lat1) * p) / 2 +
-        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
-    return 12742 *
-        asin(sqrt(a)) *
-        1000; // 2 * R; R = 6371 km, convert to meters
-  }
-
-  void _setInitialMarkers() {
-    if (!mounted) return;
-    setState(() {
-      _markers.add(
-        Marker(
-          markerId: const MarkerId('sL'),
-          position: _startLocation,
-          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-          infoWindow: const InfoWindow(title: 'Start Location'),
-        ),
-      );
-      _markers.add(
-        Marker(
-          markerId: const MarkerId('dL'),
-          position: _destinationLocation,
-          icon:
-              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-          infoWindow: const InfoWindow(title: 'Destination Location'),
-        ),
-      );
     });
   }
 
@@ -232,6 +95,38 @@ class _BigMapPageState extends State<BigMapPage> {
     });
   }
 
+  void _addMarkers() {
+    const LatLng nariBunji = LatLng(37.5121, 130.8755);
+    const LatLng dokdoObservatory = LatLng(37.4864, 130.9262);
+    const LatLng bongnaeWaterfall = LatLng(37.4983781, 130.8833256);
+    const LatLng mushrRock = LatLng(37.486811659680065, 130.80605506896973);
+
+    setState(() {
+      _markers.addAll([
+        Marker(
+          markerId: MarkerId('NariBunji'),
+          position: nariBunji,
+          infoWindow: InfoWindow(title: '나리분지'),
+        ),
+        Marker(
+          markerId: MarkerId('DokdoObservatory'),
+          position: dokdoObservatory,
+          infoWindow: InfoWindow(title: '독도 전망대'),
+        ),
+        Marker(
+          markerId: MarkerId('BongnaeWaterfall'),
+          position: bongnaeWaterfall,
+          infoWindow: InfoWindow(title: '봉래폭포'),
+        ),
+        Marker(
+          markerId: MarkerId('TurtleRock'),
+          position: mushrRock,
+          infoWindow: InfoWindow(title: '버섯바위'),
+        ),
+      ]);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -241,7 +136,7 @@ class _BigMapPageState extends State<BigMapPage> {
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: _currentPosition,
-          zoom: 14.0,
+          zoom: 11.8,
         ),
         markers: _markers,
         onMapCreated: (GoogleMapController controller) {
