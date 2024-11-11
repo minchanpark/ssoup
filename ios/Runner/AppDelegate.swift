bb19e134aeb6c5acda49738d2db9e57462d77520
minchanpark <alscksqkr13@gmail.com>
2024-11-11 17:04:48 +0900

    login, setting page 수정

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,6 @@ import Flutter
 import UIKit
 import GoogleMaps
 import Firebase
-// import kakao_flutter_sdk_common
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,16 +9,14 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    
     // Firebase 초기화
     FirebaseApp.configure()
-    
+      
     // Google Maps API 키 추가
+    GMSServices.setMetalRendererEnabled(true)
     GMSServices.provideAPIKey("AIzaSyDIBovNEZWIZSPVyRmzmBaCsInU8YgQPHc")
-    
-    // Kakao 플러그인 수동 등록
-    //SwiftKakaoFlutterSdkPlugin.register(with: self)
+
+    GeneratedPluginRegistrant.register(with: self)
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/about_login/login.dart
+++ lib/about_login/login.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk_user.dart' as kakao;
-import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:ssoup/about_login/register_page.dart';
 import '../about_home/home_navigationbar.dart';
 import '../nick_name.dart';
@@ -15,19 +14,15 @@ Future<bool> isLoginMethodMatching(String? email, String loginMethod) async {
   final CollectionReference users =
       FirebaseFirestore.instance.collection('user');
 
-  // 이메일이 일치하는 유저 문서 조회
   final QuerySnapshot result =
       await users.where('email', isEqualTo: email).get();
 
-  // 해당 이메일로 등록된 유저가 있는지 확인
   if (result.docs.isNotEmpty) {
     final userData = result.docs.first.data() as Map<String, dynamic>;
-
-    // loginMethod가 일치하는지 확인
     return userData['loginMethod'] == loginMethod;
   }
 
-  return false; // 유저가 없거나 loginMethod가 일치하지 않으면 false
+  return false;
 }
 
 Future<void> addUserToFirestore(firebase_auth.User user, String email,
@@ -68,10 +63,10 @@ Future<firebase_auth.UserCredential> signInWithKakao() async {
     print('Kakao login successful: ${token.accessToken}');
 
     final kakao.User kakaoUser = await kakao.UserApi.instance.me();
+    print("email: ${kakaoUser.kakaoAccount?.email}");
     final email = kakaoUser.kakaoAccount?.email ?? '';
     final name = kakaoUser.kakaoAccount?.profile?.nickname ?? '';
 
-    // Firestore에서 loginMethod 일치 여부 확인
     isLoginMethodMatching(email, 'Kakao');
 
     final credential = firebase_auth.OAuthProvider("oidc.kakao.com").credential(
@@ -125,7 +120,6 @@ Future<firebase_auth.UserCredential> signInWithGoogle() async {
 
     final String email = googleUser.email;
 
-    // Firestore에서 loginMethod 일치 여부 확인
     isLoginMethodMatching(email, 'Google');
 
     final credential = firebase_auth.GoogleAuthProvider.credential(
@@ -153,51 +147,6 @@ Future<firebase_auth.UserCredential> signInWithGoogle() async {
     throw firebase_auth.FirebaseAuthException(
       code: 'ERROR_GOOGLE_LOGIN_FAILED',
       message: 'Failed to login with Google: $error',
-    );
-  }
-}
-
-Future<firebase_auth.UserCredential> signInWithApple() async {
-  try {
-    final appleCredential = await SignInWithApple.getAppleIDCredential(
-      scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ],
-    );
-
-    final oauthCredential = firebase_auth.OAuthProvider("apple.com").credential(
-      idToken: appleCredential.identityToken ?? '',
-      accessToken: appleCredential.authorizationCode,
-    );
-
-    final String email = appleCredential.email ?? '';
-
-    // Firestore에서 loginMethod 일치 여부 확인
-    isLoginMethodMatching(email, 'Apple');
-
-    final userCredential = await firebase_auth.FirebaseAuth.instance
-        .signInWithCredential(oauthCredential);
-    final firebase_auth.User? user = userCredential.user;
-
-    if (user == null) {
-      throw firebase_auth.FirebaseAuthException(
-        code: 'ERROR_USER_NOT_FOUND',
-        message: 'User not found after Apple sign-in',
-      );
-    }
-
-    final String name =
-        (appleCredential.givenName ?? '') + (appleCredential.familyName ?? '');
-
-    await addUserToFirestore(user, email, name, 'Apple');
-
-    return userCredential;
-  } catch (error) {
-    print("Apple login failed: $error");
-    throw firebase_auth.FirebaseAuthException(
-      code: 'ERROR_APPLE_LOGIN_FAILED',
-      message: 'Failed to login with Apple: $error',
     );
   }
 }
@@ -260,12 +209,14 @@ class _LoginPageState extends State<LoginPage> {
     _showLoading(true);
     try {
       final userCredential = await signInWithKakao();
+      final kakao.User kakaoUser = await kakao.UserApi.instance.me();
+      final email = kakaoUser.kakaoAccount?.email ?? '';
+
       final user = userCredential.user!;
       final hasNickname = await checkNickname(user);
-      final bool isMatching =
-          await isLoginMethodMatching(userCredential.user?.email, 'Kakao');
+      final bool isMatching = await isLoginMethodMatching(email, 'Kakao');
 
-      if (isMatching == true && hasNickname) {
+      if (isMatching == true && hasNickname == true) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
@@ -284,37 +235,10 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  Future<void> _signInWithApple() async {
-    _showLoading(true);
-    try {
-      final userCredential = await signInWithApple();
-      final user = userCredential.user!;
-      final hasNickname = await checkNickname(user);
-      final bool isMatching =
-          await isLoginMethodMatching(userCredential.user?.email, 'Apple');
-
-      if (isMatching == true && hasNickname) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-              builder: (context) => const HomePageNavigationBar()),
-        );
-      } else {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const NickNamePage()),
-        );
-      }
-    } catch (e) {
-      print('Apple login error: $e');
-    } finally {
-      _showLoading(false);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     double appHeight = MediaQuery.of(context).size.height;
+    double appWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -322,13 +246,14 @@ class _LoginPageState extends State<LoginPage> {
           children: [
             SizedBox(height: (211 / 852) * appHeight),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              padding:
+                  EdgeInsets.symmetric(horizontal: (24.0 / 393) * appWidth),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   SizedBox(
-                    width: 172,
-                    height: 94,
+                    width: (172 / 393) * appWidth,
+                    height: (94 / 852) * appHeight,
                     child: Image.asset('assets/island.png'),
                   ),
                   SizedBox(height: (50 / 852) * appHeight),
@@ -336,21 +261,28 @@ class _LoginPageState extends State<LoginPage> {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.white,
                       elevation: 0,
-                      minimumSize: const Size(double.infinity, 50),
+                      minimumSize:
+                          Size(double.infinity, (50 / 852) * appHeight),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(3),
+                        borderRadius:
+                            BorderRadius.circular((3 / 393) * appWidth),
                         side: const BorderSide(width: 1),
                       ),
                     ),
                     onPressed: _isLoading ? null : _signInWithGoogle,
                     child: Row(
                       children: [
-                        const SizedBox(width: 7),
-                        Image.asset('assets/google.png', width: 25),
-                        const SizedBox(width: 60),
-                        Text('구글 계정으로 시작하기',
-                            style: regular15.copyWith(
-                                color: const Color(0xff635546))),
+                        SizedBox(width: (7 / 393) * appWidth),
+                        Image.asset('assets/google.png',
+                            width: (25 / 393) * appWidth),
+                        SizedBox(width: (60 / 393) * appWidth),
+                        Text(
+                          '구글 계정으로 시작하기',
+                          style: regular15.copyWith(
+                            color: const Color(0xff635546),
+                            fontSize: (15 / 393) * appWidth,
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -359,48 +291,24 @@ class _LoginPageState extends State<LoginPage> {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xffFAE200),
                       elevation: 0,
-                      minimumSize: const Size(double.infinity, 50),
+                      minimumSize:
+                          Size(double.infinity, (50 / 852) * appHeight),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(3),
+                        borderRadius:
+                            BorderRadius.circular((3 / 393) * appWidth),
                       ),
                     ),
                     onPressed: _isLoading ? null : _signInWithKakao,
                     child: Row(
                       children: [
-                        Image.asset('assets/kakao.png', height: 23),
-                        const SizedBox(
-                          width: 53,
-                        ),
-                        Text('카카오 계정으로 시작하기',
-                            style: regular15.copyWith(
-                                color: const Color(0xff635546))),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: (12 / 852) * appHeight),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      elevation: 0,
-                      minimumSize: const Size(double.infinity, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(3),
-                      ),
-                    ),
-                    onPressed: _isLoading ? null : _signInWithApple,
-                    child: Row(
-                      children: [
-                        const SizedBox(width: 5),
-                        const Icon(Icons.apple, size: 35, color: Colors.white),
-                        const SizedBox(width: 53),
+                        Image.asset('assets/kakao.png',
+                            height: (23 / 852) * appHeight),
+                        SizedBox(width: (53 / 393) * appWidth),
                         Text(
-                          'Apple로 로그인',
+                          '카카오 계정으로 시작하기',
                           style: regular15.copyWith(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w200,
-                            height: 0.08,
-                            letterSpacing: -0.32,
-                            fontSize: 16,
+                            color: const Color(0xff635546),
+                            fontSize: (15 / 393) * appWidth,
                           ),
                         ),
                       ],
@@ -411,9 +319,11 @@ class _LoginPageState extends State<LoginPage> {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xff919191),
                       elevation: 0,
-                      minimumSize: const Size(double.infinity, 50),
+                      minimumSize:
+                          Size(double.infinity, (50 / 852) * appHeight),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(3),
+                        borderRadius:
+                            BorderRadius.circular((3 / 393) * appWidth),
                       ),
                     ),
                     onPressed: () {
@@ -421,15 +331,20 @@ class _LoginPageState extends State<LoginPage> {
                     },
                     child: Row(
                       children: [
-                        const SizedBox(width: 7),
+                        SizedBox(width: (7 / 393) * appWidth),
                         SvgPicture.asset(
                           'assets/login.svg',
-                          width: 28,
-                          height: 28,
+                          width: (28 / 393) * appWidth,
+                          height: (28 / 852) * appHeight,
                         ),
-                        const SizedBox(width: 55),
-                        Text('아이디 비번으로 시작하기',
-                            style: regular15.copyWith(color: Colors.white)),
+                        SizedBox(width: (55 / 393) * appWidth),
+                        Text(
+                          '아이디 비번으로 시작하기',
+                          style: regular15.copyWith(
+                            color: Colors.white,
+                            fontSize: (15 / 393) * appWidth,
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -442,20 +357,22 @@ class _LoginPageState extends State<LoginPage> {
               ),
             SizedBox(height: (51 / 852) * appHeight),
             TextButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const RegisterPage()));
-                },
-                child: Text(
-                  "계정이 따로 없다면?",
-                  style: regular15.copyWith(
-                    fontWeight: FontWeight.w200,
-                    letterSpacing: -0.32,
-                    decoration: TextDecoration.underline,
-                  ),
-                ))
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const RegisterPage()));
+              },
+              child: Text(
+                "계정이 따로 없다면?",
+                style: regular15.copyWith(
+                  fontWeight: FontWeight.w200,
+                  letterSpacing: -0.32,
+                  decoration: TextDecoration.underline,
+                  fontSize: (15 / 393) * appWidth,
+                ),
+              ),
+            ),
           ],
         ),
       ),
